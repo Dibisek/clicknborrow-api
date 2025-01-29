b3e23377d74100e61f92e8d54cd03f27bd9defc2
Dibisek <114791693+Dibisek@users.noreply.github.com>
2025-01-29 16:26:12 +0100

    feat: Create factories for models

---
+++ backend/database/factories/ReservationFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $startDate = $this->faker->dateTimeBetween('-1 month', '+1 month');
+        $endDate = $this->faker->dateTimeBetween($startDate, strtotime('+1 month', $startDate->getTimestamp()));
+
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'book_id' => \App\Models\Book::factory(),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'status' => $this->faker->numberBetween(-1, 1)
+            
+        ];
+    }
+}
--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -24,10 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'phone_nb' => fake()->phoneNumber(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'is_admin' => false,
             'remember_token' => Str::random(10),
         ];
     }
