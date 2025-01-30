9ffc2904448b8e9439bd0babdc1669020711b01f
Dibisek <114791693+Dibisek@users.noreply.github.com>
2025-01-30 21:17:36 +0100

    chore: Change reservation dates to simpler format

--- backend/database/migrations/2025_01_29_142429_create_reservations_table.php
+++ backend/database/migrations/2025_01_29_142429_create_reservations_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('book_id')->constrained()->cascadeOnDelete();
-            $table->dateTime('start_date');
-            $table->dateTime('end_date');
+            $table->date('start_date');
+            $table->date('end_date');
             $table->integer('status')->default(0);
             $table->timestamps();
         });
