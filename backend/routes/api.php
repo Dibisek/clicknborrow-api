19a07629186a8ed777da2947a113fc36a57be832
Dibisek <114791693+Dibisek@users.noreply.github.com>
2025-01-30 22:19:18 +0100

    feat: Add token authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,7 +8,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'], function () {
     Route::apiResource('authors', 'AuthorController');
     Route::apiResource('books', 'BookController');
     Route::apiResource('categories', 'CategoryController');
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/setup', function () {
+    $credentials = [
+        'email' => 'admin@admin.pl',
+        'password' => 'admin'
+    ];
+
+    if (!Auth::attempt($credentials)) {
+        return 'Wrong credentials';
+    } else {
+        $user = Auth::user();
+        
+        $adminToken = $user->createToken('adminToken', ['create', 'update', 'delete', 'book:create']);
+        $userToken = $user->createToken('userToken', ['reservation:create', 'reservation:delete', 'bookmark:create', 'bookmark:delete']);
+
+        return [
+            'adminToken' => $adminToken->plainTextToken,
+            'userToken' => $userToken->plainTextToken
+        ];
+    }
 });
